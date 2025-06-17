7cf0b0c9c4e3a78a294fc74d6ef129fd512be684
Pratham <145284036+PraManGit17@users.noreply.github.com>
2025-06-18 00:25:26 +0530

    Time: 231 ms (3.29%) | Memory: 26.5 MB (72.14%) - LeetSync

--- 189-rotate-array/rotate-array.c
+++ 189-rotate-array/rotate-array.c
@@ -4,28 +4,17 @@ void reverse(int *nums, int m, int n);
 void rotate(int *nums, int numsSize, int k)
 {
   int i, j;
-
-
-
   while (numsSize < k)
   {
-    k = k - numsSize; 
+    k = k%numsSize; 
   }
-    
-
-
   for (i = 0; i < numsSize; i++)
   {
     printf("%d ", nums[i]);
   }
-
   reverse(nums, 0, numsSize - 1);
-
   reverse(nums, 0, k - 1);
-
   reverse(nums, k, numsSize - 1);
-
-  printf("\n Final: ");
   for (i = 0; i < numsSize; i++)
   {
     printf("%d ", nums[i]);
@@ -35,13 +24,11 @@ void rotate(int *nums, int numsSize, int k)
 void reverse(int *nums, int m, int n)
 {
   int temp;
-
   while (m < n)
   {
     temp = nums[m];
     nums[m] = nums[n];
     nums[n] = temp;
-    
     m++;
     n--;
   }
