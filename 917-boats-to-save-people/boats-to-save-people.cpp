909374e04600cd887d4c474739a39f05d5d8c4ff
Pratham Manjrekar <145284036+PraManGit17@users.noreply.github.com>
2025-11-02 05:22:01 +0530

    Time: 16 ms (64.98%) | Memory: 45.8 MB (68.32%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.cpp
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -1,30 +1,17 @@
-
-class Solution
-{
+class Solution {
 public:
-  int numRescueBoats(vector<int> &people, int limit)
-  {
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int left = 0, right = people.size() - 1;
+        int boats = 0;
 
-    sort(people.begin(), people.end());
-    int left = 0, right = people.size() - 1, min_count = 0, step = 1;
+        while (left <= right) {
+            if (people[left] + people[right] <= limit)
+                left++;
+            right--;
+            boats++;
+        }
 
-    while (left <= right)
-    { 
-      int sum = people[left] + people[right];
-      if (people[right] == limit || sum > limit)
-      {
-        min_count++;
-        right--;
-      }
-
-      if (sum <= limit)
-      {
-        min_count++;
-        left++;
-        right--;
-      }
+        return boats;
     }
-
-    return min_count;
-  }
 };
