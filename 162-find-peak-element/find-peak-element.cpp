d3570aaeb052a7b3fc69d8d84dbb357c030b3282
Pratham Manjrekar <145284036+PraManGit17@users.noreply.github.com>
2025-11-30 06:01:49 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (8.14%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -31,3 +31,17 @@ public:
     return 0;
   }
 };
+
+
+
+// int findPeakElement(vector<int>& nums) {
+//     int n = nums.size();
+
+//     for (int i = 0; i < n; i++) {
+//         if ((i == 0 || nums[i] > nums[i - 1]) &&
+//             (i == n - 1 || nums[i] > nums[i + 1])) {
+//             return i;
+//         }
+//     }
+//     return 0;
+// }
